4e4cdccbbf29c0258b77a68d220a8e738c849ae9
Ing. Peter Petraník <peter.petranik@bitmovin.com>
2018-11-12 23:09:32 +0100

    Initialization with create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './slobodny-vysielac-300xwhite.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://slobodnyvysielac.sk/zive-vysielanie/"
+          >
+            <img src={logo} width="300" alt="slobodny-vysielac-300xwhite" />
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
